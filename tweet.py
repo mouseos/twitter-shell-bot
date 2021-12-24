34b14e304cbf7dedb5cea216664ee5fdfd493204
yuu <46545607+mouseos@users.noreply.github.com>
2021-12-24 23:27:40 +0900

    Add files via upload
    commandstart.shをcommandstart.pyに変更。文字の改行に対応。

--- tweet.py
+++ tweet.py
@@ -4,10 +4,10 @@ import re
 
 #自身でAPIKeyを入れてね
 
-API_KEY = ''
-API_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_TOKEN_SECRET = ''
+API_KEY="3gI0OOUVZQ3cSK09IGOuaV1H8"
+API_SECRET="ijYdgwjvR4Lu9tEBwVoMtV9nx0tZUfWa4Mqgnj01wAj5mdplOJ"
+ACCESS_TOKEN="979980770633592832-MnSj8VLn7gwQ6sNMLwTmIL47GUfNNkl"
+ACCESS_TOKEN_SECRET ="HbLjY6xN0W0HVbO3mfRHCckZSVkwAhkzBar7GAboHgqGX"
 
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -15,7 +15,7 @@ api = tweepy.API(auth)
 
 #ここにTwitterアカウントのIDを入れてください(例:twitterid = 'Chromium_Linux')
 
-twitterid = ''
+twitterid = '@mouse_soft_y'
 
 class MyStream(tweepy.Stream):
  def on_status(self, status):
@@ -24,9 +24,11 @@ class MyStream(tweepy.Stream):
   name = (message)
   twitterid = (me.id_str)
   if 'シェル' in name:
-   timestamp = 'sh commandstart.sh ' +  '"' + (name) + '"'
-   print(timestamp)
-   current_time = os.popen(timestamp).readline().strip()
+   result = 'python3 commandstart.py ' +  "\"" + (name) + "\""
+   print(result , sep="\n")
+  # print("元")
+  # print(name)
+   current_time = os.popen(result).read().strip()
    print(current_time)
    api.update_status('@%s %s' % (status.user.screen_name, current_time) , in_reply_to_status_id = status.id)
   else:
